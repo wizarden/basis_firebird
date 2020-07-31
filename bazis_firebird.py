95a5b963ca97b2bba5ec106d4d923a915d5adbbc
asrock <asrock@DENIS>
2020-07-31 19:37:02 +0300

    perenos

--- bazis_firebird.py
+++ bazis_firebird.py
@@ -1,14 +1,14 @@
 import fdb as fd
 
 con11 = fd.connect(
-    dsn='d:/IL.FDB',
+    dsn='d:/Baza11.FDB',
     user='sysdba',
     password='masterkey',
     charset='UTF8'
 )
 
 con10 = fd.connect(
-    dsn='d:/my10.FDB',
+    dsn='d:/Baza10.FDB',
     user='sysdba',
     password='masterkey',
     charset='UTF8'
@@ -16,6 +16,9 @@ con10 = fd.connect(
 
 cur10 = con10.cursor()
 cur11 = con11.cursor()
+
+####################################################
+####################################################
 
 cur10.execute("delete from GROUP_MATERIAL")
 con10.commit()
@@ -26,11 +29,22 @@ for t in cur11:
     cur10.execute("insert into GROUP_MATERIAL values " + str(t).replace('None', 'null'))
 con10.commit()
 
+cur10.execute("delete from MEASURE")
+con10.commit()
+cur11.execute("select * from MEASURE")
+
+for t in cur11:
+    print(str(t).replace('None', 'null'))
+    cur10.execute("insert into MEASURE values " + str(t).replace('None', 'null'))
+con10.commit()
+
 cur10.execute("delete from MATERIAL")
 con10.commit()
 cur11.execute("select * from MATERIAL")
 
 for t in cur11:
+    print(str(t))
     print(str(t).replace('None', 'null'))
+
     cur10.execute("insert into MATERIAL values " + str(t).replace('None', 'null'))
 con10.commit()
